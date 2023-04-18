3156f9b1b6a6df04656aea6cf87a4502699d3766
Alexander <alexander9306@gmail.com>
2023-04-18 18:48:13 -0400

    added tests for the userService and userController

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,20 +1,95 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UserQueryOptionsDto } from './dto/user-query-options.dto';
+import { StatusEnum } from 'src/shared/enums/status.enum';
+import { UpdatePasswordDto } from './dto/update-password.dto';
+
+const mockUserService = () => ({
+  findAll: jest.fn(),
+  findOne: jest.fn(),
+  updateStatus: jest.fn(),
+  updatePassword: jest.fn(),
+});
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let userService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersService,
+          useFactory: mockUserService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
+    userService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  describe('findAll', () => {
+    it('should call userService.findAll with userOptionsDto', async () => {
+      const userOptionsDto = new UserQueryOptionsDto();
+      const findAllSpy = jest.spyOn(userService, 'findAll');
+
+      await controller.findAll(userOptionsDto);
+
+      expect(findAllSpy).toHaveBeenCalledWith(userOptionsDto);
+    });
+  });
+
+  describe('getUserInfo', () => {
+    it('should call userService.findOne with userId', async () => {
+      const userId = 1;
+      const findOneSpy = jest.spyOn(userService, 'findOne');
+
+      await controller.getUserInfo(userId);
+
+      expect(findOneSpy).toHaveBeenCalledWith(userId);
+    });
+  });
+
+  describe('deactivate', () => {
+    it('should call userService.updateStatus with id and StatusEnum.INACTIVE', async () => {
+      const id = '1';
+      const updateStatusSpy = jest.spyOn(userService, 'updateStatus');
+
+      await controller.deactivate(id);
+
+      expect(updateStatusSpy).toHaveBeenCalledWith(+id, StatusEnum.INACTIVE);
+    });
+  });
+
+  describe('activate', () => {
+    it('should call userService.updateStatus with id and StatusEnum.ACTIVE', async () => {
+      const id = '1';
+      const updateStatusSpy = jest.spyOn(userService, 'updateStatus');
+
+      await controller.activate(id);
+
+      expect(updateStatusSpy).toHaveBeenCalledWith(+id, StatusEnum.ACTIVE);
+    });
+  });
+
+  describe('changePassword', () => {
+    it('should call userService.updatePassword with id and updatePassDto.password', async () => {
+      const id = 1;
+      const updatePassDto = new UpdatePasswordDto();
+      const updatePasswordSpy = jest.spyOn(userService, 'updatePassword');
+
+      await controller.changePassword(id, updatePassDto);
+
+      expect(updatePasswordSpy).toHaveBeenCalledWith(
+        id,
+        updatePassDto.password,
+      );
+    });
   });
 });
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,18 +1,190 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserQueryOptionsDto } from './dto/user-query-options.dto';
+import { CollectionResultDto } from 'src/shared/dto/collection-result.dto';
+import { StatusEnum } from 'src/shared/enums/status.enum';
+import * as bcrypt from 'bcrypt';
 
+// Mock the UserRepository
+const mockUserRepository = () => ({
+  find: jest.fn(),
+  count: jest.fn(),
+  findOneBy: jest.fn(),
+  save: jest.fn(),
+});
+
+jest.mock('bcrypt');
 describe('UsersService', () => {
-  let service: UsersService;
+  let userService: UsersService;
+  let userRepository: Repository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useFactory: mockUserRepository,
+        },
+      ],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    userService = module.get<UsersService>(UsersService);
+    userRepository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('should return collection result of users', async () => {
+      const user1 = new User();
+      user1.id = 1;
+      user1.username = 'user1';
+
+      const user2 = new User();
+      user2.id = 2;
+      user2.username = 'user2';
+
+      const users = [user1, user2];
+      const count = users.length;
+      const totalPages = 1;
+
+      (userRepository as any).find.mockResolvedValue(users);
+      (userRepository as any).count.mockResolvedValue(count);
+
+      const queryOptions: UserQueryOptionsDto = {
+        pageNumber: 1,
+        pageSize: 10,
+        sortField: 'id',
+        sortDirection: 'ASC',
+      };
+
+      const result: CollectionResultDto<User> = await userService.findAll(
+        queryOptions,
+      );
+
+      expect(userRepository.find).toHaveBeenCalledWith({
+        where: {},
+        skip: 0,
+        take: queryOptions.pageSize,
+        order: { [queryOptions.sortField]: queryOptions.sortDirection },
+      });
+      expect(userRepository.count).toHaveBeenCalledWith({ where: {} });
+      expect(result).toEqual({
+        data: users,
+        pageNumber: queryOptions.pageNumber,
+        count,
+        totalPages,
+      });
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a user by id', async () => {
+      const user = new User();
+      user.id = 1;
+      user.username = 'user1';
+
+      (userRepository as any).findOneBy.mockResolvedValue(user);
+
+      const result = await userService.findOne(1);
+
+      expect(userRepository.findOneBy).toHaveBeenCalledWith({ id: 1 });
+      expect(result).toEqual(user);
+    });
+  });
+
+  describe('findOneByUserName', () => {
+    it('should return a user by username', async () => {
+      const user = new User();
+      user.id = 1;
+      user.username = 'user1';
+
+      (userRepository as any).findOneBy.mockResolvedValue(user);
+
+      const result = await userService.findOneByUserName('user1');
+
+      expect(userRepository.findOneBy).toHaveBeenCalledWith({
+        username: 'user1',
+      });
+      expect(result).toEqual(user);
+    });
+  });
+  describe('updateStatus', () => {
+    it('should update the user status', async () => {
+      const userId = 1;
+      const newStatus = StatusEnum.INACTIVE;
+      const user = new User();
+      user.id = userId;
+      user.status = StatusEnum.ACTIVE;
+
+      (userRepository as any).save.mockResolvedValue({
+        ...user,
+        status: newStatus,
+      });
+
+      const result = await userService.updateStatus(userId, newStatus);
+
+      expect(userRepository.save).toBeCalledWith({
+        ...user,
+        status: newStatus,
+      });
+      expect(result).toEqual({ ...user, status: newStatus });
+    });
+  });
+
+  describe('create', () => {
+    it('should create a new user', async () => {
+      const newUser = {
+        username: 'john.doe@example.com',
+        password: 'password123',
+      };
+      const createdUser = new User();
+      createdUser.id = 1;
+      createdUser.username = newUser.username;
+      createdUser.password = newUser.password;
+      createdUser.status = StatusEnum.ACTIVE;
+
+      (bcrypt.hash as jest.Mock).mockResolvedValue(newUser.password);
+
+      (userRepository as any).save.mockResolvedValue(createdUser);
+
+      const result = await userService.create(newUser);
+
+      expect(userRepository.save).toBeCalledWith(newUser);
+      expect(result).toEqual(createdUser);
+    });
+  });
+
+  describe('updatePassword', () => {
+    it('should update the user password', async () => {
+      const userId = 1;
+      const newPassword = 'newpassword123';
+      const user = new User();
+      user.id = userId;
+      user.password = 'oldpassword123';
+
+      (bcrypt.hash as jest.Mock).mockResolvedValue(newPassword);
+      (userRepository as any).save.mockResolvedValue({
+        ...user,
+        password: newPassword,
+      });
+
+      const result = await userService.updatePassword(userId, newPassword);
+
+      expect(userRepository.save).toBeCalledWith({
+        ...user,
+        password: newPassword,
+      });
+      expect(result).toEqual({
+        ...user,
+        password: newPassword,
+      });
+    });
   });
 });
