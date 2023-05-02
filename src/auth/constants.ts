3f17e777dceece54b516cc413a3a5368e9bf7403
Alexander <alexander9306@gmail.com>
2023-05-02 13:06:27 -0400

    change default ignore expiration value

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -2,6 +2,6 @@ import 'dotenv/config';
 
 export const jwtConstants = {
   secret: process.env.JWT_SECRET ?? 'MySecretKey',
-  expiresIn: process.env.JWT_EXPIRATION_TIME ?? '1h',
-  ignoreExpiration: process.env.NODE_ENV === 'development',
+  expiresIn: process.env.JWT_EXPIRATION_TIME ?? '60s',
+  ignoreExpiration: false,
 };
